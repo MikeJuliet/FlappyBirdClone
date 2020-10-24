aa5afacf587c5f94ebd202f3dcc9ed608878adc5
MikeJuliet <marthinusjacobs@gmail.com>
2020-10-24 13:48:27 +0200

    Converted to Kotlin

---
+++ app/src/main/java/com/morteza/flappybirdclone/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.morteza.flappybirdclone
+
+import android.os.*
+import androidx.appcompat.app.*
+
+/**
+ * Created by MJ on 2020/10/24 at 13:45
+ */
+class MainActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
