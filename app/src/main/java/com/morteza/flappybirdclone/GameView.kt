ef5eec909f20bd4b8e6f3149b4a0a9507ae84472
MikeJuliet <marthinusjacobs@gmail.com>
2020-10-24 15:07:59 +0200

    Move the bird

--- app/src/main/java/com/morteza/flappybirdclone/GameView.kt
+++ app/src/main/java/com/morteza/flappybirdclone/GameView.kt
@@ -9,8 +9,16 @@ import android.view.*
  */
 class GameView(context : Context) : View(context) {
 
+    //Canvas
+    private var canvasWidth : Int = 0
+    private var canvasHeight : Int = 0
+
     //Bird
-    private var bird : Bitmap? = null
+    //    private var bird : Bitmap? = null
+    private var bird : ArrayList<Bitmap> = ArrayList(2)
+    private val birdX = 10
+    private var birdY = 0
+    private var birdSpeed = 0
 
     //Background
     private var backgroundImage : Bitmap? = null
@@ -22,10 +30,15 @@ class GameView(context : Context) : View(context) {
     private var levelPaint = Paint()
 
     //Life
-    private var life : ArrayList<Bitmap>? = null
+    private var life : ArrayList<Bitmap> = ArrayList(2)
+
+    //Status check
+    private var touchFlag : Boolean = false
 
     init {
-        bird = BitmapFactory.decodeResource(resources, R.drawable.bird1)
+        bird.add(BitmapFactory.decodeResource(resources, R.drawable.bird1))
+        bird.add(BitmapFactory.decodeResource(resources, R.drawable.bird2))
+
         backgroundImage = BitmapFactory.decodeResource(resources, R.drawable.bg)
 
         scorePaint.color = Color.BLACK
@@ -39,24 +52,52 @@ class GameView(context : Context) : View(context) {
         levelPaint.textAlign = Paint.Align.CENTER
         levelPaint.isAntiAlias = true
 
-        life = ArrayList(2)
-        life!!.add(BitmapFactory.decodeResource(resources, R.drawable.heart))
-        life!!.add(BitmapFactory.decodeResource(resources, R.drawable.heart_g))
+        life.add(BitmapFactory.decodeResource(resources, R.drawable.heart))
+        life.add(BitmapFactory.decodeResource(resources, R.drawable.heart_g))
+
+        // Bird first position
+        birdY = 500
     }
 
     override fun onDraw(canvas : Canvas?) {
         super.onDraw(canvas)
 
+        canvasWidth = width
+        canvasHeight = height
+
         canvas?.drawBitmap(backgroundImage!!, 0F, 0F, null)
 
-        canvas?.drawBitmap(bird!!, 0F, 0F, null)
+        //  Bird
+        val minBirdY = bird[0].height
+        val maxBirdY = canvasHeight - bird[0].height * 3
+
+        birdY += birdSpeed
+        if (birdY < minBirdY) birdY = minBirdY
+        if (birdY > maxBirdY) birdY = maxBirdY
+        birdSpeed += 2
+
+        if (touchFlag){
+            //  Flap wings
+            canvas?.drawBitmap(bird[1], birdX.toFloat(), birdY.toFloat(), null)
+            touchFlag = false
+        }else{
+            canvas?.drawBitmap(bird[0], birdX.toFloat(), birdY.toFloat(), null)
+        }
 
         canvas?.drawText("Score : 0", 20F, 60F, scorePaint)
 
         canvas?.drawText("Lv.1", (width / 2).toFloat(), 60F, levelPaint)
 
-        canvas?.drawBitmap(life!![0], 790F, 30F, null)
-        canvas?.drawBitmap(life!![0], 880F, 30F, null)
-        canvas?.drawBitmap(life!![1], 970F, 30F, null)
+        canvas?.drawBitmap(life[0], 790F, 30F, null)
+        canvas?.drawBitmap(life[0], 880F, 30F, null)
+        canvas?.drawBitmap(life[1], 970F, 30F, null)
+    }
+
+    override fun onTouchEvent(event : MotionEvent?) : Boolean {
+        if (event?.action == MotionEvent.ACTION_DOWN){
+            touchFlag = true
+            birdSpeed = -20
+        }
+        return true
     }
 }
--- app/src/main/java/com/morteza/flappybirdclone/MainActivity.kt
+++ app/src/main/java/com/morteza/flappybirdclone/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.morteza.flappybirdclone
 
 import android.os.*
-import android.widget.*
 import androidx.appcompat.app.*
+import java.util.*
+import kotlin.concurrent.*
 
 /**
  * Created by MJ on 2020/10/24 at 13:45
  */
-class MainActivity:AppCompatActivity() {
-    private var gameView:GameView? = null
+class MainActivity : AppCompatActivity() {
+
+    private var gameView : GameView? = null
+    private var handler : Handler = Handler()
+
+    companion object {
+
+        private const val TIMER_INTERVAL : Long = 30
+    }
 
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +24,14 @@ class MainActivity:AppCompatActivity() {
         //Setting up gameView
         gameView = GameView(this)
         setContentView(gameView)
+
+        val timer = Timer()
+        timer.schedule(timerTask {
+            kotlin.run {
+                handler.post {
+                    gameView!!.invalidate()
+                }
+            }
+        }, 0, TIMER_INTERVAL)
     }
 }
